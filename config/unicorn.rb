be083d5f42458e7860f68ce7b702a18c625374a1
Jay McGavren <jay@mcgavren.com>
2017-02-20 22:15:05 -0700

    Update paths in configuration.

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -1,6 +1,6 @@
-pid "/home/deploy/guestbook/pids/unicorn.pid"
-stderr_path "/home/deploy/guestbook/unicorn/unicorn.log"
-stdout_path "/home/deploy/guestbook/unicorn/unicorn.log"
+pid "/home/deploy/guestbook/current/pids/unicorn.pid"
+stderr_path "/home/deploy/guestbook/current/unicorn/unicorn.log"
+stdout_path "/home/deploy/guestbook/current/unicorn/unicorn.log"
 
 listen "/tmp/unicorn.guestbook.sock"
 worker_processes 2
--- config/unicorn_init.sh
+++ config/unicorn_init.sh
@@ -14,7 +14,7 @@ set -e
 
 # Feel free to change any of the following variables for your app:
 TIMEOUT=${TIMEOUT-60}
-APP_ROOT=/home/x/my_app/current
+APP_ROOT=/home/deploy/guestbook/current
 PID=$APP_ROOT/tmp/pids/unicorn.pid
 CMD="cd $APP_ROOT && bundle exec unicorn -D -c $APP_ROOT/config/unicorn.rb -E production"
 INIT_CONF=$APP_ROOT/config/init.conf
